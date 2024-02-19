cc17d72a1be392d692dd517527036dd07868f2e2
Mohammad Hasan Pourbakhsh <mh.pourbakhsh@gmail.com>
2024-02-19 01:08:06 -0800

    Changing the Logo

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -9,8 +9,8 @@ import { useGetGenresQuery } from '../../services/TMDB';
 import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 import genreIcons from '../../assets/genres';
 
-const redLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
-const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
+const blueLogo = '/images/cinema-paradiso-blue-logo-transparent.svg';
+const redLogo = '/images/cinema-paradiso-red-logo-transparent.svg';
 
 const categories = [
   { label: 'Popular', value: 'popular' },
@@ -34,8 +34,8 @@ function Sidebar({ setMobileOpen }) {
       <Link to="/" className={classes.imageLink}>
         <img
           className={classes.image}
-          src={theme.palette.mode === 'light' ? redLogo : blueLogo}
-          alt="Filmpire Logo"
+          src={theme.palette.mode === 'light' ? blueLogo : redLogo}
+          alt="Cnema Paradiso Logo"
         />
       </Link>
       <Divider />
